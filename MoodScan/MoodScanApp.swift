3faa83e437576fa994a03145ba1192e973e56b51
emillymaia <emillypereira1290@gmail.com>
2023-07-29 16:09:40 -0300

    Initial Commit

---
+++ MoodScan/MoodScanApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoodScanApp.swift
+//  MoodScan
+//
+//  Created by Emilly Maia on 29/07/23.
+//
+
+import SwiftUI
+
+@main
+struct MoodScanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
